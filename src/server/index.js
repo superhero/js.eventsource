a50d7c89a813531b4ec6204c6f8d4061a7755108
superhero <erik@landvall.se>
2021-02-21 23:24:47 +0100

    cleaned up adding layers

--- src/server/index.js
+++ src/server/index.js
@@ -3,8 +3,8 @@ const
   coreFactory = new CoreFactory,
   core        = coreFactory.create()
 
-core.add('eventsource', __dirname)
 core.add('client/redis', '@superhero/core.redis/src/client')
+core.add('server', __dirname)
 core.add('mapper', __dirname + '/../mapper')
 core.add('schema', __dirname + '/../schema')
 
